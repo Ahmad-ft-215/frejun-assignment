3e02d22e24ca759a2b21808959bf868ea05712c6
Ahmad Noor <99959745+Ahmad-ft-215@users.noreply.github.com>
2022-10-21 10:49:15 +0530

    completed frejun assignment

--- routes/Blog.routes.js
+++ routes/Blog.routes.js
@@ -20,17 +20,29 @@ blogController.get("/blogs", async (req, res) => {
 })
 
 
-blogController.post("/:_id", async (req, res) => {
+blogController.get("/:_id", async (req, res) => {
     var id = req.params._id;
     const blog = await BlogModel.find({ "_id": id });
     if(!blog) {
         return res.send("can't find blog")
     }
-    let arr = [];
+    let arr = blog[0].body.split(" ");
     
-    //console.log(blog[0].title)
-    return res.send({ "blog": blog, "body": blog[0].body.split(" ") })
+    const words = arr.filter((wrd)=>wrd[0].toLowerCase()=='a');
+    return res.send({ "blog": blog , "post body starting with letter a or A": words})
     
 })
+
+
+blogController.patch('/edit/:_id',async(req,res)=>{
+    try{
+     const blog= await BlogModel.findByIdAndUpdate(req.params._id, req.body);
+     await blog.save();
+     return res.send(blog);
+    }
+    catch(err){
+     return res.status(500).send({message:err.message});
+    }
+ })
 
 module.exports = blogController;
